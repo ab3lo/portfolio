c5389febb234b4a22d8be2273cb4995eb99e90d7
ab3lo <drneutrium@protonmail.com>
2026-08-31 20:51:50 +0500

    minor tweaks

--- app/components/home/Faq.vue
+++ app/components/home/Faq.vue
@@ -1,44 +1,37 @@
 <script setup lang="ts">
-import { faq } from '~/data/portfolio'
+import { faq } from "~/data/portfolio";
 
 const items = faq.map((group) => {
   return {
     label: group.title,
     key: group.title.toLowerCase(),
     questions: group.questions,
-  }
-})
+  };
+});
 
 const ui = {
-  root: 'flex items-center gap-4 w-full',
-  list: 'relative flex bg-transparent dark:bg-transparent gap-2',
-  indicator: 'absolute top-[4px] duration-200 ease-out focus:outline-none rounded-full bg-white/10 dark:bg-neutral-900',
+  root: "flex items-center gap-4 w-full",
+  list: "relative flex bg-transparent dark:bg-transparent gap-2",
+  indicator:
+    "absolute top-[4px] duration-200 ease-out focus:outline-none rounded-full bg-white/10 dark:bg-neutral-900",
   trigger: [
-    'relative inline-flex items-center justify-center flex-shrink-0 focus:outline-none transition-colors duration-200 ease-out border-white/10 border-2',
-    'px-3 py-2 font-medium rounded-full',
-    'hover:bg-neutral-900/80',
-    'data-[state=active]:text-highlighted',
-    'data-[state=inactive]:text-muted',
+    "relative inline-flex items-center justify-center flex-shrink-0 focus:outline-none transition-colors duration-200 ease-out border-white/10 border-2",
+    "px-3 py-2 font-medium rounded-full",
+    "hover:bg-neutral-900/80",
+    "data-[state=active]:text-highlighted",
+    "data-[state=inactive]:text-muted",
   ],
-  label: 'truncate',
-}
+  label: "truncate",
+};
 </script>
 
 <template>
   <div class="flex w-full flex-col items-center justify-center space-y-8 sm:px-20 md:px-30">
     <div class="flex flex-col items-center justify-center gap-2">
-      <h3 class="font-newsreader italic text-white-shadow text-4xl">
-        FAQ
-      </h3>
-      <p class="text-center text-sm font-medium text-muted">
-        Things I get asked a lot
-      </p>
+      <h3 class="font-newsreader italic text-white-shadow text-4xl">FAQ</h3>
+      <p class="text-center text-sm font-medium text-muted">Common Inquires</p>
     </div>
-    <UTabs
-      :items
-      orientation="horizontal"
-      :ui
-    >
+    <UTabs :items orientation="horizontal" :ui>
       <template #content="{ item }">
         <UAccordion
           :unmount-on-hide="false"
